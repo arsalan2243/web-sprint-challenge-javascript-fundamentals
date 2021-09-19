ef6e817af03cef1301a8b6a5c1604164f9f7a57c
arsalan rahimi <years1373@yahoo.com>
2021-09-19 03:19:43 -0700

    task 1 and 2 are completed

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+// when we define a function within another function the inner function can access the variable within the outer function. (this is where the closure happens and this is called lexical scope function)
+// we can see that internal variable is defind out side of nested function and the nested function can acces it 
 
 
 
@@ -28,11 +29,16 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
+function summation(num) {
+  let counter = 0;
+  for (let i = 0; i <= num; i++) {
+    counter += i;
+  }
+  return counter;
 
   }
- 
+  
+console.log( summation(5))
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
